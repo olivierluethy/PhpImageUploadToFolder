b9044ad1bf6a1b88b29d316cbac1cbdcffc8338e
Olivier_Luethy <olivier.luethy@gmx.net>
2023-02-01 18:51:03 +0100

    Finished

--- index.php
+++ index.php
@@ -1,50 +1,14 @@
 <?php
-// Check if form has been submitted
-if(isset($_POST['submit'])){
-  // Get file information from $_FILES array
-  $file = $_FILES['file'];
-  
-  $fileName = $_FILES['file']['name'];
-  $fileTmpName = $_FILES['file']['tmp_name'];
-  $fileSize = $_FILES['file']['size'];
-  $fileError = $_FILES['file']['error'];
-  $fileType = $_FILES['file']['type'];
+// Connect to the database
+$conn = mysqli_connect("localhost", "root", "", "babylow");
 
-  // Get the file extension
-  $fileExt = explode('.', $fileName);
-  $fileActualExt = strtolower(end($fileExt));
+// Fetch all the image paths from the database
+$sql = "SELECT path FROM images";
+$result = mysqli_query($conn, $sql);
 
-  // List of allowed file extensions
-  $allowed = array('jpg', 'jpeg', 'png', 'gif');
-
-  // Check if the file extension is allowed
-  if (in_array($fileActualExt, $allowed)) {
-    // Check for any errors during the upload
-    if ($fileError === 0) {
-      // Check if the file size is within limits
-      if ($fileSize < 1000000) {
-        // Generate a unique file name
-        $fileNameNew = uniqid('', true).".".$fileActualExt;
-        // Define the destination folder and file name
-        $fileDestination = 'uploads/'.$fileNameNew;
-        // Move the uploaded file to the destination folder
-        move_uploaded_file($fileTmpName, $fileDestination);
-        // Redirect to index page with success message
-        header("Location: index.php?uploadsuccess");
-      } else {
-        echo "Your file is too big!";
-      }
-    } else {
-      echo "There was an error uploading your file!";
+// Loop through the result and display each image
+while ($row = mysqli_fetch_array($result)) {
+    $path = $row['path'];
+    echo "<img src='$path' style='width:200px;height:200px;margin:10px;'>";
     }
-  } else {
-    echo "You cannot upload files of this type!";
-  }
-}
-?>
-
-<!-- HTML form to select and upload a file -->
-<form action="upload.php" method="post" enctype="multipart/form-data">
-  <input type="file" name="file">
-  <button type="submit" name="submit">Upload</button>
-</form>
+    ?>
---
+++ upload.php
@@ -0,0 +1,61 @@
+<?php
+// Connect to the database
+$conn = mysqli_connect("localhost", "root", "", "babylow");
+
+// Check if the "images" folder exists, if not create it
+if (!file_exists("images")) {
+  mkdir("images", 0777, true);
+}
+
+// Check if form has been submitted
+if(isset($_POST['submit'])){
+  // Get file information from $_FILES array
+  $file = $_FILES['file'];
+  
+  $fileName = $_FILES['file']['name'];
+  $fileTmpName = $_FILES['file']['tmp_name'];
+  $fileSize = $_FILES['file']['size'];
+  $fileError = $_FILES['file']['error'];
+  $fileType = $_FILES['file']['type'];
+
+  // Get the file extension
+  $fileExt = explode('.', $fileName);
+  $fileActualExt = strtolower(end($fileExt));
+
+  // List of allowed file extensions
+  $allowed = array('jpg', 'jpeg', 'png', 'gif');
+
+  // Check if the file extension is allowed
+  if (in_array($fileActualExt, $allowed)) {
+    // Check for any errors during the upload
+    if ($fileError === 0) {
+      // Check if the file size is within limits
+      if ($fileSize < 1000000) {
+        // Generate a unique file name
+        $fileNameNew = uniqid('', true).".".$fileActualExt;
+        // Define the destination folder and file name
+        $fileDestination = 'images/'.$fileNameNew;
+        // Move the uploaded file to the destination folder
+        move_uploaded_file($fileTmpName, $fileDestination);
+        // Insert image path into the database
+        $sql = "INSERT INTO images (path) VALUES ('$fileDestination')";
+        mysqli_query($conn, $sql);
+        // Redirect to index page with success message
+        header("Location: index.php?uploadsuccess");
+      } else {
+        echo "Your file is too big!";
+      }
+    } else {
+      echo "There was an error uploading your file!";
+    }
+  } else {
+    echo "You cannot upload files of this type!";
+  }
+}
+?>
+
+<!-- HTML form to select and upload a file -->
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <button type="submit" name="submit">Upload</button>
+</form>
